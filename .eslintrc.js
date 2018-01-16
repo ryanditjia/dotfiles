aaa7563fdfabde8e7a789655d63000ae3201b579
Ryandi Tjia <ryandi.tjia@gmail.com>
2018-01-16 16:50:47 +0700

    Cleaner eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -51,16 +51,37 @@ module.exports = {
   rules: {
     // No semi
     semi: ['error', 'never'],
-    // Prefer single, but backticks ok
+
+    // setting max line length to 80
+    'max-len': ['error', { code: 80 }],
+
+    // always require parens around arrow function params
+    'arrow-parens': ['error', 'always'],
+
+    // prefer single, but backticks ok
     quotes: ['error', 'single', { allowTemplateLiterals: true }],
+
     // This rule requires return statements to either always or never specify values
     'consistent-return': ['error'],
+
     // .js for React is ok
     'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+
+    // self explanatory
+    // 'react/prefer-stateless-function': ['off'],
+
+    // self explanatory
+    'react/prop-types': ['off'],
+
     // "as-needed" enforces no braces where they can be omitted
     // If option set to true, it requires braces and an explicit return for object literals.
-    'arrow-body-style': ['error', 'as-needed', { requireReturnForObjectLiteral: true }],
-    // Allows to as valid alternative to href
+    'arrow-body-style': [
+      'error',
+      'as-needed',
+      { requireReturnForObjectLiteral: true },
+    ],
+
+    // Allows "to" as valid alternative to href
     'jsx-a11y/anchor-is-valid': [
       'error',
       {
